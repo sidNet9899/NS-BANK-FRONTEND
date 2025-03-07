11b78da479581db5d4c5290dbe8643a6bf96a282
sidNet9899 <sgotmare@netfotech.in>
2025-03-07 15:34:09 +0530

    made page responsive for all types of screens

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FiMenu, FiX } from "react-icons/fi"; // Import menu and close icons
 import "./Navbar.css";
 import logo from "../images/logo.png"; 
 import loginIcon from "../images/login.png"; 
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo-container">
         <img src={logo} alt="Bank Logo" className="logo-img" />
         <span className="bank-name-heading">BANKNAME</span>
       </div>
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/features">Feature</Link></li>
-        <li><Link to="/chat">AccessChat</Link></li>
+
+      {/* Hamburger Icon - Only Visible on Mobile & Tablet */}
+      <div className="menu-icon" onClick={toggleMenu}>
+        {menuOpen ? <FiX size={32} /> : <FiMenu size={32} />}
+      </div>
+
+      {/* Navigation Links */}
+      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+        <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
+        <li><Link to="/about" onClick={() => setMenuOpen(false)}>About</Link></li>
+        <li><Link to="/services" onClick={() => setMenuOpen(false)}>Services</Link></li>
+        <li><Link to="/features" onClick={() => setMenuOpen(false)}>Feature</Link></li>
+        <li><Link to="/chat" onClick={() => setMenuOpen(false)}>AccessChat</Link></li>
       </ul>
+
+      {/* Login Icon */}
       <img src={loginIcon} alt="Login" className="login-icon" />
     </nav>
   );
